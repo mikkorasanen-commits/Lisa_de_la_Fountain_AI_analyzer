4cfda20116f182ed5d379d746345a4106f0ee993
mikkorasanen-commits <mikko.rasanen@compile.fi>
2025-09-25 11:32:59 +0000

    v.0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
+from PIL import Image
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+logo = Image.open("lisa_logo.png")
+st.markdown(
+    "<div style='position:absolute;top:20px;right:30px;z-index:10;'>",
+    unsafe_allow_html=True
 )
+st.image(logo, width=160)
+st.markdown("</div>", unsafe_allow_html=True)
